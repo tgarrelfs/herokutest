459fb06025c4e2be3fd4cc41c901b36665a0e6e7
Tom <t.garrelfs@kimplusdelta.nl>
2017-01-20 18:05:03 +0100

    again

--- testapp/apps.py
+++ testapp/apps.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 import pyodbc
 import django-pyodbc-azure
+import bestaatniet
 
 class TestappConfig(AppConfig):
     name = 'testapp'
